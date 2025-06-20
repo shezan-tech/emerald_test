bac713abcdb7bc63793bdf1d30d76cb0dfe25b78
shezan-tech <ariekechield22@gmail.com>
2025-06-20 17:37:46 +0700

    Update lineage_emerald.mk

--- lineage_emerald.mk
+++ lineage_emerald.mk
@@ -14,15 +14,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from tanzanite device
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
 
-PRODUCT_DEVICE := tanzanite
-PRODUCT_NAME := lineage_tanzanite
+PRODUCT_DEVICE := emerald
+PRODUCT_NAME := lineage_emerald
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi Note 14 4G
+PRODUCT_MODEL := Redmi Note 13 pro 4g
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.1.0.VOGMIXM release-keys" \
-    BuildFingerprint=Redmi/tanzanite_n_global/tanzanite:15/AP3A.240905.015.A2/OS2.0.1.0.VOGMIXM:user/release-keys
+    PRIVATE_BUILD_DESC="missi-user 15 AP3A.240905.015.A2 OS2.0.6.0.VNFMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/missi/missi:15/AP3A.240905.015.A2/OS2.0.6.0.VNFMIXM:user/release-keys
 
